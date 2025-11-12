953aeb19aadbe6a922ef9ebe4bfa1f9fb776ab1e
TwsitedWizard <saranshabu@gmail.com>
2025-11-12 22:02:01 +0530

    Initial commit: Spring Boot project with Web and Thymeleaf

---
+++ src/main/java/com/nexussearch/nexussearch/NexusSearchApplication.java
@@ -0,0 +1,13 @@
+package com.nexussearch.nexussearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexussearch/nexussearch/NexusSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexussearch.nexussearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
